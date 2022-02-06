9fa64b3736caf2c60989b8c5f50f02cb66d9ecb5
YliaVaininlovich <Ylia.v@tut.by>
2022-02-06 13:53:20 +0300

    feat:create fuction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var tmp = [];
+  var flag = true;
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] != ")" && str[i] != "]" && str[i] != "}") {
+      tmp.push(str[i]);
+    } else {
+      if (str[i] == ")" && tmp[tmp.length - 1] == "(")
+        tmp.splice(tmp.length - 1, 1);
+      else if (str[i] == "]" && tmp[tmp.length - 1] == "[")
+        tmp.splice(tmp.length - 1, 1);
+      else if (str[i] == "}" && tmp[tmp.length - 1] == "{")
+        tmp.splice(tmp.length - 1, 1);
+      else {
+        flag = false;
+        break;
+      }
+    }
+
+    if (
+      tmp[tmp.length - 1] != "(" &&
+      tmp[tmp.length - 1] != "[" &&
+      tmp[tmp.length - 1] != "{"
+    ) {
+      flag = false;
+      for (var i = 0; i < bracketsConfig.length; i++)
+        if (
+          tmp[tmp.length - 2] == bracketsConfig[i][0] &&
+          tmp[tmp.length - 1] == bracketsConfig[i][1]
+        ) {
+          tmp.splice(tmp.length - 2, 2);
+          flag = true;
+        }
+    }
+  }
+  if (tmp.length > 0) flag = false;
+
+  return flag;
+};
