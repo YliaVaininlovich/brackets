f1ee07194b1153a95c548ddf366332acd81d2f53
YliaVaininlovich <Ylia.v@tut.by>
2022-02-06 14:53:37 +0300

    feat: 20 passing

--- src/index.js
+++ src/index.js
@@ -4,35 +4,33 @@ module.exports = function check(str, bracketsConfig) {
   for (var i = 0; i < str.length; i++) {
     if (str[i] != ")" && str[i] != "]" && str[i] != "}") {
       tmp.push(str[i]);
-    } else {
-      if (str[i] == ")" && tmp[tmp.length - 1] == "(")
-        tmp.splice(tmp.length - 1, 1);
-      else if (str[i] == "]" && tmp[tmp.length - 1] == "[")
-        tmp.splice(tmp.length - 1, 1);
-      else if (str[i] == "}" && tmp[tmp.length - 1] == "{")
-        tmp.splice(tmp.length - 1, 1);
-      else {
-        flag = false;
-        break;
-      }
+    } else if (str[i] == ")" && tmp[tmp.length - 1] == "(")
+      tmp.splice(tmp.length - 1, 1);
+    else if (str[i] == "]" && tmp[tmp.length - 1] == "[")
+      tmp.splice(tmp.length - 1, 1);
+    else if (str[i] == "}" && tmp[tmp.length - 1] == "{")
+      tmp.splice(tmp.length - 1, 1);
+    else {
+      flag = false;
     }
 
     if (
-      tmp[tmp.length - 1] != "(" &&
+      (tmp[tmp.length - 1] == tmp[tmp.length - 2] && tmp[tmp.length - 1]) !=
+        "(" &&
       tmp[tmp.length - 1] != "[" &&
       tmp[tmp.length - 1] != "{"
     ) {
-      flag = false;
-      for (var i = 0; i < bracketsConfig.length; i++)
+      for (var j = 0; j < bracketsConfig.length; j++)
         if (
-          tmp[tmp.length - 2] == bracketsConfig[i][0] &&
-          tmp[tmp.length - 1] == bracketsConfig[i][1]
+          tmp[tmp.length - 2] == bracketsConfig[j][0] &&
+          tmp[tmp.length - 1] == bracketsConfig[j][1]
         ) {
           tmp.splice(tmp.length - 2, 2);
           flag = true;
         }
     }
   }
+
   if (tmp.length > 0) flag = false;
 
   return flag;
